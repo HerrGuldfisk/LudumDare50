1ea3e543340a16a323265f813effd394e61f9d60
Theodor Hennings <41120242+HerrGuldfisk@users.noreply.github.com>
2022-04-02 11:40:29 +0200

    Fireplace

---
+++ LudumDare50/Assets/Scripts/Fireplace/Fireplace.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fireplace : MonoBehaviour
+{
+    [Header("Components")]
+    public Animator anim;
+    public Slider fuelSlider;
+
+    [Header("Fire Data")]
+    public float factor = 1;
+    
+    public float fuel = 0;
+
+    public float maxFuel = 60f;
+
+    public bool burning;
+
+
+    void Start()
+    {
+        // anim.Play("Burning");
+    }
+
+
+    void Update()
+    {
+        // Don't update if no fuel.
+        if(fuel <= 0) { return; }
+
+        // Starting burn animation.
+        if (!burning)
+        {
+            anim.Play("Burning");
+            burning = true;
+        }
+
+        // Burning
+        fuel -= factor * Time.deltaTime;
+
+        fuelSlider.value = fuel / maxFuel;
+
+        // Checking fuel
+        if(fuel <= 0)
+        {
+            fuel = 0;
+            burning = false;
+            anim.Play("Idle");
+        }
+
+    }
+
+    public void AddFireWood(float amount)
+    {
+        fuel += amount;
+        Mathf.Clamp(fuel, 0, maxFuel);
+    }
+}
